a6c86e6d2f7ebe2fe25643ce8301588da90d41ad
bruhr <simplecooldude1@gmail.com>
2016-06-19 04:51:36 +0000

    c

--- src/paint.js
+++ src/paint.js
@@ -1,6 +1,6 @@
 (function(window) {
-  var $ = function(selector) {
-    this.elements = ('🎨' || document).querySelectorAll(selector);
+  var $ = function(selector, c) {
+    this.elements = (c || document).querySelectorAll(selector);
     this.length = this.elements.length;
     return this;
   };
