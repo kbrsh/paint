9810ce1e98469bfd895d5ceef7211cad159863f1
bruhr <simplecooldude1@gmail.com>
2016-06-19 04:23:12 +0000

    new paint file, to be merged into regular

---
+++ src/newpaint.js
@@ -0,0 +1,68 @@
+(function(window) {
+  var $ = function(selector, context) {
+    this.elements = (context || document).querySelectorAll(selector);
+    this.length = this.elements.length;
+    return this;
+  };
+
+  $.fn = $.prototype = {
+    ready: function(callback) {
+      document.addEventListener("DOMContentLoaded", callback);
+    },
+
+    each: function(callback) {
+      for (var i = 0; i < this.length; i++) {
+        callback(this.elements[i], i, this.elements);
+      }
+      return this;
+    },
+
+    attr: function(name, val) {
+      if (val) {
+        this.setAttribute(name, val);
+      } else if (!val) {
+        return this.getAttribute(name);
+      }
+      return this;
+    },
+
+    addClass: function(className) {
+      this.each(function(element) {
+        element.classList.add(className);
+      });
+      return this;
+    },
+
+    removeClass: function(className) {
+      this.each(function(element) {
+        element.classList.remove(className);
+      });
+
+      return this;
+    },
+    
+    val: function(val) {
+      if(val) {
+        this.value = val;
+      } else if(!val) {
+        return this.elements[0].value;
+      }
+      return this;
+    },
+    
+    html: function(html) {
+      if(html) {
+        this.each(function() {
+          this.innerHTML = html;
+        });
+      } else if(!html) {
+        return this.innerHTML;
+      }
+      return this;
+    }
+  };
+
+  window.$ = function(selector, context) {
+    return new $(selector, context);
+  };
+})(window);
